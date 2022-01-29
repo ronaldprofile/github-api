4802f5d8ee6b7c1e523efd28caf0d4a530a0259a
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-01-29 15:07:21 -0300

    added dependencie react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { Home } from "./pages/Home";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AppRoutes } from "./Routes";
 import { GlobalStyles } from "./styles/Global";
 
 function App() {
   return (
-    <>
+    <Router>
+      <AppRoutes />
       <GlobalStyles />
-      <Home />
-    </>
+    </Router>
   );
 }
 
